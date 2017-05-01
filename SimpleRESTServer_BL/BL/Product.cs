67624fd05a9d5af830cec3c7b96b9be954e120be
capaul1977 <capaul1977@gmail.com>
2017-05-01 09:10:39 -0400

    Initial Upload

---
+++ SimpleRESTServer_BL/BL/Product.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleRESTServer_BL.BL
+{
+
+    public abstract class AbsctractProductFactory
+    {
+        public abstract AbstractHeadwear CreateHeadwear();
+        public abstract AbstractNovelty CreateNovelty();
+        public abstract AbstractApparel CreateApparel();
+        public abstract AbstractService CreateService();
+        public abstract AbstractAccessory CreateAccessory();
+        public abstract AbstractStoreSupply CreateStoreSupply();
+
+    }
+
+    public abstract class AbstractProduct
+    {
+        public string SKU { get; set; }
+        public string Description { get; set; }
+        public ProductStatus Status { get; set; }
+    }
+
+    public enum ProductStatus
+    {
+        Active, Inactive
+    }
+
+    public class ConcreteProductFactory : AbsctractProductFactory
+    {
+        public override AbstractAccessory CreateAccessory()
+        {
+           return new Accessory();
+        }
+
+        public override AbstractApparel CreateApparel()
+        {
+            return new Apparel();
+        }
+
+        public override AbstractHeadwear CreateHeadwear()
+        {
+            return new Headwear();
+        }
+
+        public override AbstractNovelty CreateNovelty()
+        {
+            return new Novelty();
+        }
+
+        public override AbstractService CreateService()
+        {
+            return new Service();
+        }
+
+        public override AbstractStoreSupply CreateStoreSupply()
+        {
+            return new StoreSupply();
+        }
+    }
+
+    public abstract class AbstractHeadwear : AbstractProduct
+    {
+        public string Size { get; set; }
+    }
+    public abstract class AbstractNovelty : AbstractProduct { }
+    public abstract class AbstractApparel : AbstractProduct
+    {
+        public string Size { get; set; }
+    }
+    public abstract class AbstractService : AbstractProduct { }
+    public abstract class AbstractAccessory : AbstractProduct { }
+    public abstract class AbstractStoreSupply : AbstractProduct { }
+
+    public class Headwear : AbstractHeadwear { }
+    public class Novelty : AbstractNovelty { }
+    public class Apparel : AbstractApparel { }
+    public class Service : AbstractService { }
+    public class Accessory : AbstractAccessory { }
+    public class StoreSupply : AbstractStoreSupply { }
+
+
+
+}
---
+++ SimpleRESTServer_UnitTests/BL/Product.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleRESTServer_BL.BL;
+
+namespace SimpleRESTServer_UnitTests.BL
+{
+    [TestClass]
+    public class Product
+    {
+        
+        
+
+        [TestMethod]
+        public void RequestForHeadwearObjectShouldReturnHeadwearObject()
+        {
+
+            //Arrange
+            AbsctractProductFactory pf = new ConcreteProductFactory();
+
+            //Act
+            AbstractHeadwear h = pf.CreateHeadwear();
+            h.Size = "1";
+
+            //Assert
+            Assert.AreEqual("SimpleRESTServer_BL.BL.Headwear", h.GetType().ToString());
+        }
+
+        [TestMethod]
+        public void RequestForNoveltyObjectShouldReturnNoveltyObject()
+        {
+            //Arrange
+            AbsctractProductFactory pf = new ConcreteProductFactory();
+
+            //Act
+            AbstractNovelty h = pf.CreateNovelty();
+            h.SKU = "12345";
+
+            //Assert
+            Assert.AreEqual("SimpleRESTServer_BL.BL.Novelty", h.GetType().ToString());
+        }
+
+        [TestMethod]
+        public void RequestForApparelObjectShouldReturnApparelObject()
+        {
+            //Arrange
+            AbsctractProductFactory pf = new ConcreteProductFactory();
+
+            //Act
+            AbstractApparel h = pf.CreateApparel();
+            h.SKU = "12345";
+
+            //Assert
+            Assert.AreEqual("SimpleRESTServer_BL.BL.Apparel", h.GetType().ToString());
+        }
+
+        [TestMethod]
+        public void RequestForServiceObjectShouldReturnServiceObject()
+        {
+            //Arrange
+            AbsctractProductFactory productfactory = new ConcreteProductFactory();
+
+            //Act
+            AbstractService service = productfactory.CreateService();
+
+            //Assert
+            Assert.AreEqual("SimpleRESTServer_BL.BL.Service", service.GetType().ToString());
+
+        }
+
+        [TestMethod]
+        public void RequestForAccessoryObjectShouldReturnAccessoryObject()
+        {
+            //Arrange
+            AbsctractProductFactory pf = new ConcreteProductFactory();
+
+            //Act
+            AbstractAccessory h = pf.CreateAccessory();
+            h.SKU = "12345";
+
+            //Assert
+            Assert.AreEqual("SimpleRESTServer_BL.BL.Accessory", h.GetType().ToString());
+        }
+
+        [TestMethod]
+        public void RequestForStoreSupplyObjectShouldReturnStoreSupplyObject()
+        {
+            //Arrange
+            AbsctractProductFactory pf = new ConcreteProductFactory();
+
+            //Act
+            AbstractStoreSupply h = pf.CreateStoreSupply();
+            h.SKU = "12345";
+
+            //Assert
+            Assert.AreEqual("SimpleRESTServer_BL.BL.StoreSupply", h.GetType().ToString());
+        }
+
+        [TestMethod]
+        public void RequestForNoveltyObjectShouldReturnProductObject()
+        {
+            //Arrange
+            AbsctractProductFactory ProductFactory = new ConcreteProductFactory();
+
+            //Act
+            AbstractProduct NoveltyItem = ProductFactory.CreateNovelty();
+            NoveltyItem.SKU = "12345";
+
+            //Assert
+            Assert.AreEqual("SimpleRESTServer_BL.BL.Novelty", NoveltyItem.GetType().ToString());
+        }
+
+        [TestMethod]
+        public void ShouldBeAbleToSetAndGetSizeForHeadwearProduct()
+        {
+            //Arrange
+            AbsctractProductFactory productFactory = new ConcreteProductFactory();
+
+            //Act
+            AbstractProduct HeadwearItem = productFactory.CreateHeadwear();
+
+            ((AbstractHeadwear)HeadwearItem).Size = "13";
+            
+
+            //Assert
+            Assert.AreEqual("13", ((AbstractHeadwear)HeadwearItem).Size);
+        }
+
+        [TestMethod]
+        public void ShouldBeAbleToSetAndGetSizeForApparelProduct()
+        {
+            //Arrange
+            AbsctractProductFactory ProductFactory = new ConcreteProductFactory();
+
+            //Act
+            AbstractApparel ApparelItem = ProductFactory.CreateApparel();
+            ApparelItem.Size = "1.5";
+
+            //Assert
+            Assert.AreEqual("1.5", ApparelItem.Size);
+        }
+
+        [TestMethod]
+        public void ShouldBeAbleToSetAndGetSKUForAnyProduct()
+        {
+            //Arrange
+            AbsctractProductFactory ProductFactory = new ConcreteProductFactory();
+            string sku = "12345";
+
+            //Act
+            AbstractApparel Item = ProductFactory.CreateApparel();
+            Item.SKU = sku;
+
+            //Assert
+            Assert.AreEqual(sku, Item.SKU);
+        }
+
+
+        [TestMethod]
+        public void ShouldBeAbleToSetAndGetProductStatusForAnyProduct()
+        {
+            //Arrange
+            AbsctractProductFactory ProductFactory = new ConcreteProductFactory();
+            ProductStatus productstatus = ProductStatus.Active;
+
+            //Act
+            AbstractApparel Item = ProductFactory.CreateApparel();
+            Item.Status = productstatus;
+
+            //Assert
+            Assert.AreEqual(productstatus, Item.Status);
+        }
+
+        [TestMethod]
+        public void ShouldBeAbleToSetAndGetBasePropertiesForAnyProduct()
+        {
+            //Arrange
+            AbsctractProductFactory ProductFactory = new ConcreteProductFactory();
+            string description = "Test Description";
+
+            //Act
+            AbstractApparel Item = ProductFactory.CreateApparel();
+            Item.Description = description;
+
+            //Assert
+            Assert.AreEqual(description, Item.Description);
+        }
+    }
+}
